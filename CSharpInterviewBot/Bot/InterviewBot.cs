0523863d486888a73cef13d7dcf606ad6ad3cc57
Claude Will <claude.will@yahoo.com>
2025-01-11 21:24:02 -0600

    Redo of Initial Commit -completed work failed to push.

---
+++ CSharpInterviewBot/Bot/InterviewBot.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpInterviewBot.Bot
+{
+    public class InterviewBot
+    {
+        private InterviewQuestions interviewQuestions;
+        private Introduction introduction
+        private int currentTopicIndex;
+        private int currentQuestionIndex;
+        private List<string> currentTopicKeys;
+
+        public InterviewBot()
+        {
+            interviewQuestions = new InterviewQuestions();
+            introduction = new Introduction();
+            currentTopicKeys = new List<string>(interviewQuestions.QuestionsByTopic.Keys);
+            currentTopicIndex = 0;
+            currentQuestionIndex = 0;
+        }
+
+        public async Task StartInterview()
+        {
+            introduction.SpeakIntroduction();
+            Console.WriteLine("Let's start the interview!");
+            await AskQuestion();
+        }
+    }
+}
---
+++ CSharpInterviewBot/Program.cs
@@ -0,0 +1,12 @@
+﻿using CSharpInterviewBot.Bot;
+using System.Security.Cryptography.X509Certificates;
+
+public class Program
+{
+    public static async Task Main(string[] args)
+    {
+        
+        InterviewBot bot = new InterviewBot();
+        await bot.StartInterview();
+    }
+}
